fe24db1ebb121a8689094389fdbbca67975031ed
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-05-07 20:01:15 +0200

    add link to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,26 @@ const Navbar = () => {
         >
           <img src='src/assets/logo.png' alt="logo" className="w-9
           h-9 object-contain" />
+          <p className="text-white text-[18px] 
+          font-bold cursor-pointer">Lukasz <span className="sm:block hidden">| Junior Fullstack Developer</span></p>
         </Link>
+        <ul className="list-none hidden sm:flex
+        flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title
+                  ? "text-white"
+                  : "text-secondary"
+              } hover:text-white text-[18px]
+              font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
