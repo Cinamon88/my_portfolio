31dd0080b390e54b88c39d2e10956ac2abfa2745
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-05-18 23:01:46 +0200

    ignore eslint

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/prop-types */
+/* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/no-unknown-property */
-import React from 'react'
+
 import { Tilt } from 'react-tilt';
 import { motion } from 'framer-motion';
 import { styles } from '../style';
