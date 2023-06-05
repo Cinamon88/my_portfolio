2fd3ba0e837e12f79940114e309909fa28b6df6b
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-06-05 18:44:39 +0200

    minor changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,13 +12,13 @@ const Hero = () => {
       gap-5`}>
         <div className="flex flex-col justify-center 
         items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          <div className="w-5 h-5 rounded-full bg-[#27ae60]" />
+          <div className="w-1 sm:h-80 h-40" />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm 
-          <span className="text-[#915eff]"> Łukasz</span>
+          <span className="text-[#27ae60]"> Łukasz</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop fullstack web applications.
